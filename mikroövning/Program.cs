8bfbc2956cf9dd52b51aa4eb465ea31e1e99ff9e
Richard Ekholm <richard.ekholm@elev.ga.ntig.se>
2024-10-14 09:46:12 +0200

    gjorde så pengarna dras beronde på vad man köper mm

--- mikroövning/Program.cs
+++ mikroövning/Program.cs
@@ -7,9 +7,9 @@ int pengar = 100;
 
 
     Console.WriteLine("Vilket föremål skulle du vilja tillförskaffa idag?🥺");
-    Console.WriteLine("Val.1 En fin diamant ring👀");
-    Console.WriteLine("Val.2 Simons bodypillow🤤");
-    Console.WriteLine("Val.3 Ludvigs lurviga ludd💖");
+    Console.WriteLine("Val.1 En fin diamant ring👀, 𝓽𝓳𝓾𝓰𝓸 𝓴𝓻𝓸𝓷𝓸𝓻");
+    Console.WriteLine("Val.2 Simons bodypillow🤤, 𝓽𝓻𝓮𝓽𝓽𝓲𝓸 𝓯𝓮𝓶 𝓴𝓻𝓸𝓷𝓸𝓻");
+    Console.WriteLine("Val.3 Ludvigs lurviga ludd💖, 𝓯𝔂𝓻𝓽𝓲𝓸 𝓴𝓻𝓸𝓷𝓸𝓻");
     string jhonpork = "";   
 
     while   (jhonpork != "1" && jhonpork != "2" && jhonpork != "3")
@@ -48,17 +48,23 @@ int antalnum = 0;
 bool success = int.TryParse(antal, out antalnum);
 
 
+
 if (success == false)
+
 
 {
 Console.WriteLine("Hur många vill köpa med en siffra 😡");
 }
 
-if(success == true)
-{
-    Console.WriteLine("Okej det funkar bombaclatt");
+
+if( pengar < (prisjhonpork * antalnum)) {
+    Console.WriteLine("DET GÅR INTE");
+}
+else {
+pengar = pengar - prisjhonpork * antalnum;
 }
 
+Console.WriteLine($"Superb val min vän.\ndu har {pengar} kr kvar");
 
 Console.ReadLine();
 
